ab4398e63a1be50dd7e4226635ffcbf849836eb6
Dave Clark <dave.clark@theiconic.com.au>
2017-12-15 15:20:21 +1100

    Corrected pins

--- main/main.ino
+++ main/main.ino
@@ -1,12 +1,12 @@
 const int TRIGGER = 4; //12
 const int ECHO = 7; //13
 const int MAXIMUM_VALUE = 255;
-const int MOTOR2_FORWARD = 13;
-const int MOTOR2_BACKWARD = 12;
+const int MOTOR2_FORWARD = 12;
+const int MOTOR2_BACKWARD = 13;
 const int MOTOR2_VELOCITY = 11;
-const int MOTOR1_FORWARD = 8;
-const int MOTOR1_BACKWARD = 7;
-const int MOTOR1_VELOCITY = 6;
+const int MOTOR1_FORWARD = 9;
+const int MOTOR1_BACKWARD = 8;
+const int MOTOR1_VELOCITY = 10;
 const int RIGHT_SENSOR = A2;
 const int MIDDLE_SENSOR = A1;
 const int LEFT_SENSOR = A0;
@@ -50,57 +50,73 @@ int sensor_get_distance() {
   delayMicroseconds(10);
   digitalWrite(TRIGGER, LOW);
   int duration = pulseIn(ECHO, HIGH);
-  int distance = (duration / 2) * SPEED_OF_SOUND;
+  double distance = (duration / 2) * SPEED_OF_SOUND;
+  Serial.println("Distance:");
+  Serial.println(distance);
+  Serial.println("Duration:");
+  Serial.println(duration);
   return distance;
 }
 
 void forward(){
   forward_wheel_left();
   forward_wheel_right();
-//  delay(1000);
+}
+
+void do_stop(){
+  forward_wheel_left_stop();
+  forward_wheel_right_stop();
 }
 
 void back(){
   backward_wheel_left();
   backward_wheel_right();
-//  delay(1000);
 }
   
 void forward_wheel_right(){
   digitalWrite(MOTOR1_FORWARD, HIGH);
   digitalWrite(MOTOR1_BACKWARD, LOW);
-  analogWrite(MOTOR1_VELOCITY, 20); //This value need to be from 0-255
-//  delay(1000);
+  analogWrite(MOTOR1_VELOCITY, 100); //This value need to be from 0-255
 }
+
 void forward_wheel_left(){
   digitalWrite(MOTOR2_FORWARD, HIGH);
   digitalWrite(MOTOR2_BACKWARD, LOW);
-  analogWrite(MOTOR2_VELOCITY, 170); //This value need to be from 0-255
-//  delay(1000);
+  analogWrite(MOTOR2_VELOCITY, 100); //This value need to be from 0-255
+}
+
+void forward_wheel_right_stop(){
+  digitalWrite(MOTOR1_FORWARD, LOW);
+  digitalWrite(MOTOR1_BACKWARD, LOW);
+  analogWrite(MOTOR1_VELOCITY, 0); //This value need to be from 0-255
+}
+
+void forward_wheel_left_stop(){
+  digitalWrite(MOTOR2_FORWARD, LOW);
+  digitalWrite(MOTOR2_BACKWARD, LOW);
+  analogWrite(MOTOR2_VELOCITY, 0); //This value need to be from 0-255
 }
 
 void backward_wheel_right(){
-  // Backward
   digitalWrite(MOTOR1_FORWARD, LOW);
   digitalWrite(MOTOR1_BACKWARD, HIGH);
-  analogWrite(MOTOR1_VELOCITY, 20); //This value need to be from 0-255
-//  delay(1000);
+  analogWrite(MOTOR1_VELOCITY, 100); //This value need to be from 0-255
 }
   
 void backward_wheel_left(){
   digitalWrite(MOTOR2_FORWARD, LOW);
   digitalWrite(MOTOR2_BACKWARD, HIGH);
-  analogWrite(MOTOR2_VELOCITY, 170); //This value need to be from 0-255
-//  delay(1000);
+  analogWrite(MOTOR2_VELOCITY, 100); //This value need to be from 0-255
 }
 
 void update_sensor_values() {
-    previous_left_sensor = current_left_sensor;
-    current_left_sensor = analogRead(LEFT_SENSOR);
-    previous_middle_sensor = current_middle_sensor;
-    current_middle_sensor = analogRead(MIDDLE_SENSOR);
-    previous_right_sensor = current_right_sensor;
-    current_right_sensor = analogRead(RIGHT_SENSOR);
+    sensor_get_distance();
+//    previous_left_sensor = current_left_sensor;
+//    current_left_sensor = analogRead(LEFT_SENSOR);
+//    previous_middle_sensor = current_middle_sensor;
+//    current_middle_sensor = analogRead(MIDDLE_SENSOR);
+//    previous_right_sensor = current_right_sensor;
+//    current_right_sensor = analogRead(RIGHT_SENSOR);
 }
 
 void update_state() {
@@ -108,47 +124,38 @@ void update_state() {
     
     if (enemy_detected()) {
       current_state = STATE_ENEMY_ATTACK;
-   }
+    }
 }
 
 void execute_strategy() {    
+    Serial.println(current_state);
     if (current_state == STATE_ENEMY_FIND) {
         // find enemy
-       
+       delay(1000);
     }
 
     if (current_state == STATE_ENEMY_ATTACK) {
         // move forward
+       forward();
        
+       delay(1000);
     }
 
     if (current_state == STATE_AVOID_GOING_OUTSIDE) {
         // move away from tape
-         
+        back();
+        forward();
+        delay(300);
+        do_stop();
+        back();
+        delay(300);
+        do_stop();
     }
 }
 
 void loop() {
-//  update_sensor_values();
+  update_sensor_values();
 //  update_state();
 //  execute_strategy();
-  
-  forward();
-  back();
-  
-//  int distance = sensor_get_distance();
-//    Serial.println(distance);
-//    Serial.println("LEFT_SENSOR");
-//    Serial.println(analogRead(LEFT_SENSOR));
-//
-//    Serial.println("MIDDLE_SENSOR");
-//    Serial.println(analogRead(MIDDLE_SENSOR));
-//
-//    Serial.println("RIGHT_SENSOR");
-//    Serial.println(analogRead(RIGHT_SENSOR));
-    
-  
-
-
-    
+//  Serial.println(current_state);
 }
